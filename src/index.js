8f330497c4b0b24b1359cb3c6c24958220fb095f
Dzianis Sheka <dzianis.sheka@gmail.com>
2019-03-23 13:25:31 +0300

    feat: add stack (14 / 20)

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 // start with the first case
 // start with second case
 // third case need to count closing / open brackets
+// intruduce refactoring / stack
 
 module.exports = function check(str, bracketsConfig) {
   // const config1 = [['(', ')']];
@@ -14,23 +15,38 @@ module.exports = function check(str, bracketsConfig) {
       }),
       closed: true
     };
-  })
+  });
+
+  let globalClose = true;
+  let lastOpened = [];
 
   // const config2 = bracketsConfig[1].map((sign)=> {
   //   return { sign, count: 0 };
   // });
 
   const chars = str.split('');
+  console.log('chars', chars);
+  
+
+  const checkOpenBracket = (config, char) => {
+    return (config[0].sign === char && config[1].count === config[0].count);
+  }
 
   const checkConfig = (configs, char) => {
     configs.forEach((bracketConfig) => {
       const config = bracketConfig.bracketConfig;
-      if (config[0].sign === char && config[1].count === config[0].count) {
+      const isPossibleToOpen = checkOpenBracket(config, char);
+      const isOtherClosed = configs.every(config => config.closed);
+      if (isPossibleToOpen) {
         config[0].count += 1; 
+        lastOpened.unshift(char);
         bracketConfig.closed = false;
+      } else if (isPossibleToOpen) {
+        // globalClose = false
       }
-      if (config[1].sign === char && (config[0].count - config[1].count) === 1) {
+      if (config[0].sign === lastOpened[0] && config[1].sign === char && (config[0].count - config[1].count) === 1) {
         config[1].count += 1; 
+        lastOpened.shift();
         bracketConfig.closed = true;
       }
     });
@@ -40,7 +56,7 @@ module.exports = function check(str, bracketsConfig) {
     checkConfig(configs, char);
   });
 
-  console.log('configs', JSON.stringify(configs, 0,2));
+  console.log('configs', JSON.stringify(configs, 0, 2));
 
-  return configs.every(config => config.closed);
+  return configs.every(config => config.closed) && globalClose;
 }
